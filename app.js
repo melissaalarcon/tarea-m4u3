b12c9bd358328562da64bf82bf49508aa3eaedc2
Melissa Alarcón <mel.massai24@gmail.com>
2023-10-17 18:31:37 -0300

    tarea completada

---
+++ app.js
@@ -0,0 +1,63 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var nosotrosRouter = require('./routes/nosotros');
+var serviciosRouter = require('./routes/servicios');
+var contactoRouter = require('./routes/contacto');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('./nosotros', nosotrosRouter);
+app.use('./servicios', serviciosRouter);
+app.use('./contacto', contactoRouter);
+
+app.get('/prueba', function(req,res) {
+  res.send('Esta es la pagina de prueba')
+});
+
+app.get('/nosotros', function(req,res) {
+  res.send('Esta es la seccion nosotros')
+});
+
+app.get('/servicios', function(req,res) {
+  res.send('Esta es la seccion de servicios')
+});
+
+app.get('/contacto', function(req,res) {
+  res.send('Esta es la seccion de contacto')
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
